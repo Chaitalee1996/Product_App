e7fb9739070fc63c408bbdf39fc5209a15abd204
Chaitalee1996 <kudalechaitale5@gmail.com>
2023-07-26 18:47:17 +0530

    define type to route params

--- src/screen/ProductDetails.tsx
+++ src/screen/ProductDetails.tsx
@@ -4,9 +4,13 @@ import {Button, Text, View} from 'react-native'
 import { useRoute } from "@react-navigation/native";
 import {GetProductDetailsAPI} from '../api/Products/productListAPI'
 import ProductDetailsItem from "../components/ProdcutDetailsItem/productDetailsItemIndex";
+import { RouteProp } from '@react-navigation/native';
 const ProductDetails=()=>{
-   
-    const route=useRoute()
+    type IncidentRouteParams = {
+        productId: String;
+      }
+    const route = useRoute<RouteProp<Record<string, IncidentRouteParams>, string>>();
+    // const route=useRoute()
     const productId=route.params.productId
     console.log("productId,",productId)
      const [productDetails,setProductDetails]=useState([])
